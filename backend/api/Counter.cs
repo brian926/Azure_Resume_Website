aea8a1c4589d2e11385b87c6ebd0b483a392f260
Brian Antunes <brian.antunes@yahoo.com>
2022-03-27 20:55:35 -0400

    Updated Counter

--- backend/api/Counter.cs
+++ backend/api/Counter.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 
 namespace Company.Function {
+    // Creating the Counter Class with Id Count and Time as properties
     public class Counter {
         [JsonProperty(PropertyName="id")] public string Id {get; set;}
         [JsonProperty(PropertyName="count")] public int Count {get; set;}
