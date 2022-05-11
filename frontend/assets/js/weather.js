74f593d4bf1d2f54eca76c2d9d33126712aca264
Brian Antunes <brian.antunes@yahoo.com>
2022-05-10 20:54:47 -0400

    Updated Weather feature

--- frontend/assets/js/weather.js
+++ frontend/assets/js/weather.js
@@ -3,46 +3,11 @@ const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
-/*SUBSCRIBE HERE FOR API KEY: https://home.openweathermap.org/users/sign_up*/
-const apiKey = "";
+const apiKey = "d6b7a3447529c297169c609d74cda367";
 
-form.addEventListener("submit", e => {
+document.addEventListener('DOMContentLoaded', e => {
     e.preventDefault();
-    let inputVal = input.value;
-
-    //check if there's already a city
-    const listItems = list.querySelectorAll(".ajax-section .city");
-    const listItemsArray = Array.from(listItems);
-
-    if (listItemsArray.length > 0) {
-        const filteredArray = listItemsArray.filter(el => {
-            let content = "";
-            //athens,gr
-            if (inputVal.includes(",")) {
-                //athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
-                if (inputVal.split(",")[1].length > 2) {
-                    inputVal = inputVal.split(",")[0];
-                    content = el
-                        .querySelector(".city-name span")
-                        .textContent.toLowerCase();
-                } else {
-                    content = el.querySelector(".city-name").dataset.name.toLowerCase();
-                }
-            } else {
-                //athens
-                content = el.querySelector(".city-name span").textContent.toLowerCase();
-            }
-            return content == inputVal.toLowerCase();
-        });
-
-        if (filteredArray.length > 0) {
-            msg.textContent = `You already know the weather for ${filteredArray[0].querySelector(".city-name span").textContent
-                } ...otherwise be more specific by providing the country code as well 😉`;
-            form.reset();
-            input.focus();
-            return;
-        }
-    }
+    let inputVal = "Boston";
 
     //ajax here
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
@@ -72,7 +37,7 @@ form.addEventListener("submit", e => {
             list.appendChild(li);
         })
         .catch(() => {
-            msg.textContent = "Please search for a valid city 😩";
+            msg.textContent = "Can't find a valid city";
         });
 
     msg.textContent = "";
