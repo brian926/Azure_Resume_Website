ab06a3f4d2f5cbd9c9824fa91a22a95de5bf3ef6
Brian Antunes <brian.antunes@yahoo.com>
2022-05-16 21:39:41 -0400

    Updated Frontend weather feature

--- frontend/assets/js/weather.js
+++ frontend/assets/js/weather.js
@@ -1,16 +1,20 @@
 ﻿/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
-const form = document.querySelector(".top-banner form");
-const input = document.querySelector(".top-banner input");
-const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
-const apiKey = "d6b7a3447529c297169c609d74cda367";
+const apiKey = "";
+const api = ""
 
 document.addEventListener('DOMContentLoaded', e => {
     e.preventDefault();
-    let inputVal = "Boston";
+
+    $.get(`http://ipinfo.io?token=${api}`, function (response) {
+
+        var loc = response.loc;
+        var myArray = loc.split(",");
+        let lat = myArray[0];
+        let log = myArray[1];
 
     //ajax here
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${apiKey}&units=imperial`;
 
     fetch(url)
         .then(response => response.json())
@@ -18,6 +22,7 @@ document.addEventListener('DOMContentLoaded', e => {
             const { main, name, sys, weather } = data;
             const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]
                 }.svg`;
+            console.log(icon);
 
             const li = document.createElement("li");
             li.classList.add("city");
@@ -35,12 +40,6 @@ document.addEventListener('DOMContentLoaded', e => {
       `;
             li.innerHTML = markup;
             list.appendChild(li);
-        })
-        .catch(() => {
-            msg.textContent = "Can't find a valid city";
         });
-
-    msg.textContent = "";
-    form.reset();
-    input.focus();
+    }, "jsonp");
 });
