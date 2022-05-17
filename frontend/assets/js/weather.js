15caafc35270a43be5953ad99f27422c90c67de5
Brian Antunes <brian.antunes@yahoo.com>
2022-05-17 18:50:06 -0400

    Updated weather.js

--- frontend/assets/js/weather.js
+++ frontend/assets/js/weather.js
@@ -1,7 +1,6 @@
-﻿/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
-const list = document.querySelector(".ajax-section .cities");
+﻿const list = document.querySelector(".ajax-section .cities");
 const apiKey = "";
-const api = ""
+const api = "";
 
 document.addEventListener('DOMContentLoaded', e => {
     e.preventDefault();
@@ -13,33 +12,42 @@ document.addEventListener('DOMContentLoaded', e => {
         let lat = myArray[0];
         let log = myArray[1];
 
-    //ajax here
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${apiKey}&units=imperial`;
+        //Multi City here
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${apiKey}&units=imperial`;
+        var cityList = [];
+        cityList.push(url);
 
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const { main, name, sys, weather } = data;
-            const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]
-                }.svg`;
-            console.log(icon);
-
-            const li = document.createElement("li");
-            li.classList.add("city");
-            const markup = `
-        <h4 class="city-name" data-name="${name},${sys.country}">
-          <span>${name}</span>
-          <sup>${sys.country}</sup>
-        </h4>
-        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
-        <figure>
-          <img class="city-icon" src="${icon}" alt="${weather[0]["description"]
-                }">
-          <figcaption>${weather[0]["description"]}</figcaption>
-        </figure>
-      `;
-            li.innerHTML = markup;
-            list.appendChild(li);
+        var defaultCities = ["new%20york", "san%20francisco", "nashville", "boston"];
+        defaultCities.forEach(element => {
+            cityList.push(`https://api.openweathermap.org/data/2.5/weather?q=${element},us&appid=${apiKey}&units=imperial`);
         });
+
+        for (const url of cityList) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const { main, name, sys, weather } = data;
+                const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`;
+                console.log(icon);
+
+                const li = document.createElement("li");
+                li.classList.add("city");
+                const markup = `
+                            <h4 class="city-name" data-name="${name},${sys.country}">
+                              <span>${name}</span>
+                              <sup>${sys.country}</sup>
+                            </h4>
+                            <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+                            <figure>
+                              <img class="city-icon" src="${icon}" alt="${weather[0]["description"]
+                                        }">
+                              <figcaption>${weather[0]["description"]}</figcaption>
+                            </figure>
+                          `;
+                li.innerHTML = markup;
+                list.appendChild(li);
+             });
+        };
     }, "jsonp");
+    
 });
