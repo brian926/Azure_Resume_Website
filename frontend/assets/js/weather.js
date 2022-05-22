9b20df57336e4b2e8d9aa683d93edab540a28a72
Brian Antunes <brian.antunes@yahoo.com>
2022-05-21 21:45:40 -0400

    Another typo

--- frontend/assets/js/weather.js
+++ frontend/assets/js/weather.js
@@ -1,5 +1,5 @@
 ﻿const list = document.querySelector(".ajax-section .cities");
-const funcUrl = 'https://test2weatherfun.azurewebsites.net/api/WeatherFunction?';
+const funcUrl = 'https://test2weatherfun.azurewebsites.net/api/WeatherFunction';
 
 document.addEventListener('DOMContentLoaded', e => {
     e.preventDefault();
